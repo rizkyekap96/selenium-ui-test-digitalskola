bc7f3106d9bda6601c82dc654defc0b1b93bab74
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-22 22:38:47 +0700

    fix

--- login-headless.js
+++ login-headless.js
@@ -3,48 +3,82 @@ const chrome = require("selenium-webdriver/chrome");
 const assert = require("assert");
 
 async function Login() {
+  describe("Login Test", function () {
+    it("TC01 - Login Success", async function () {
 
-  // Menambahkan Chrome Option
-  let options = new chrome.Options();
-  options.addArguments("--headless");
+      let options = new chrome.Options();
+      options.addArguments("--headless");
 
-  //Membuat koneksi dengan webdriver
-  let driver = await new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(options)
-    .build();
-  try {
-    //login
-    await driver.get("https://www.saucedemo.com/");
-    await driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    await driver
-      .findElement(By.xpath("//input[@id='password']"))
-      .sendKeys("secret_sauce");
-    await driver.findElement(By.name("login-button")).click();
+      let driver = await new Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(options)
+        .build();
+      try {
+        await driver.get("https://www.saucedemo.com/");
+        await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        await driver
+          .findElement(By.xpath("//input[@id='password']"))
+          .sendKeys("secret_sauce");
+        await driver.findElement(By.name("login-button")).click();
 
-    //validasi user berada di dashboard
-    let titleText = await driver
-      .findElement(By.xpath("//div[@class='app_logo']"))
-      .getText();
-    assert.strictEqual(
-      titleText.includes("Swag Lab"),
-      true,
-      "Title does not include Swag Lab"
-    );
+        let titleText = await driver
+          .findElement(By.xpath("//div[@class='app_logo']"))
+          .getText();
+        assert.strictEqual(
+          titleText.includes("Swag Lab"),
+          true,
+          "Title does not include Swag Lab"
+        );
 
+        //add item to cart
+        await driver
+          .findElement(By.id("add-to-cart-sauce-labs-backpack"))
+          .click();
 
-    //add item to cart
-    await driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        //validate item sukses ditambahkan ke cart
+        let addCart = await driver
+          .findElement(By.xpath("//span[@class='shopping_cart_badge']"))
+          .getText();
+        assert.strictEqual(addCart > 0, true, "No item on cart");
+      } finally {
+        setTimeout(async () => {
+          await driver.quit();
+        }, 5000);
+      }
+    }),
+      it("TC02 - Login Failed", async function () {
+        let options = new chrome.Options();
+      options.addArguments("--headless");
 
-    //validate item sukses ditambahkan ke cart
-    let addCart = await driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText();
-    assert.strictEqual(addCart > 0, true, "No item on cart");
-   }
- finally {
-    setTimeout(async () => {
-      await driver.quit();
-    }, 5000);
-  }
+      let driver = await new Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(options)
+        .build();
+      try {
+        await driver.get("https://www.saucedemo.com/");
+        await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        await driver
+          .findElement(By.xpath("//input[@id='password']"))
+          .sendKeys("secret_saucee");
+        await driver.findElement(By.name("login-button")).click();
+
+        let titleText = await driver
+          .findElement(By.css(".error-message-container"))
+          .getText();
+        assert.strictEqual(
+          titleText.includes(
+            "Epic sadface: Username and password do not match any user in this service"
+          ),
+          true,
+          "Error message"
+        );
+      } finally {
+        setTimeout(async () => {
+          await driver.quit();
+        }, 5000);
+      }
+      });
+  });
 }
 
 Login();
