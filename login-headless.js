b544b7a0d5399e3e25c12240d0eb7e2465cef0dc
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-22 21:22:46 +0700

    login headless

---
+++ login-headless.js
@@ -0,0 +1,52 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const assert = require("assert");
+
+async function Login() {
+
+  // Menambahkan Chrome Option
+  let options = new chrome.Options();
+  options.addArguments("--headless");
+
+  //Membuat koneksi dengan webdriver
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
+  try {
+    //login
+    await driver.get("https://www.saucedemo.com/");
+    await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    await driver
+      .findElement(By.xpath("//input[@id='password']"))
+      .sendKeys("secret_sauce");
+    await driver.findElement(By.name("login-button")).click();
+
+    //validasi user berada di dashboard
+    let titleText = await driver
+      .findElement(By.xpath("//div[@class='app_logo']"))
+      .getText();
+    assert.strictEqual(
+      titleText.includes("Swag Lab"),
+      true,
+      "Title does not include Swag Lab"
+    );
+    console.log("Login success!")
+
+
+    //add item to cart
+    await driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+
+    //validate item sukses ditambahkan ke cart
+    let addCart = await driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText();
+    assert.strictEqual(addCart > 0, true, "No item on cart");
+   }
+ finally {
+    setTimeout(async () => {
+      await driver.quit();
+    }, 5000);
+  }
+}
+
+Login();
