bb8d0e276b13ed15fcf5e1bdaef8263ca740fe0a
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-22 23:11:03 +0700

    fix

--- login-headless.js
+++ login-headless.js
@@ -77,7 +77,99 @@ async function Login() {
           await driver.quit();
         }, 5000);
       }
-      });
+      }),
+      it ("TC 03 - Add item to cart", async function () {
+        let options = new chrome.Options();
+      options.addArguments("--headless");
+
+      let driver = await new Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(options)
+        .build();
+      try {
+        await driver.get("https://www.saucedemo.com/");
+        await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        await driver
+          .findElement(By.xpath("//input[@id='password']"))
+          .sendKeys("secret_sauce");
+        await driver.findElement(By.name("login-button")).click();
+
+        let titleText = await driver
+          .findElement(By.xpath("//div[@class='app_logo']"))
+          .getText();
+        assert.strictEqual(
+          titleText.includes("Swag Lab"),
+          true,
+          "Title does not include Swag Lab"
+        );
+
+        //add item to cart
+        await driver
+          .findElement(By.id("add-to-cart-sauce-labs-backpack"))
+          .click();
+
+        //validate item sukses ditambahkan ke cart
+        let addCart = await driver
+          .findElement(By.xpath("//span[@class='shopping_cart_badge']"))
+          .getText();
+        assert.strictEqual(addCart > 0, true, "No item on cart");
+      } finally {
+        setTimeout(async () => {
+          await driver.quit();
+        }, 5000);
+      }
+      }),
+      it ("TC 04 - Cart no item", async function () {
+        this.timeout(10000)
+        let options = new chrome.Options();
+      options.addArguments("--headless");
+
+      let driver = await new Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(options)
+        .build();
+      try {
+        await driver.get("https://www.saucedemo.com/");
+        await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        await driver
+          .findElement(By.xpath("//input[@id='password']"))
+          .sendKeys("secret_sauce");
+        await driver.findElement(By.name("login-button")).click();
+
+        let titleText = await driver
+          .findElement(By.xpath("//div[@class='app_logo']"))
+          .getText();
+        assert.strictEqual(
+          titleText.includes("Swag Lab"),
+          true,
+          "Title does not include Swag Lab"
+        );
+
+        //add item to cart
+        await driver
+          .findElement(By.id("add-to-cart-sauce-labs-backpack"))
+          .click();
+
+          await driver
+          .findElement(By.id("shopping_cart_container"))
+          .click();
+
+        //validate item sukses ditambahkan ke cart
+        let isHasItem = await driver
+          .findElement(By.css(".btn.btn_secondary.btn_small.cart_button"))
+          .getText();
+          
+        assert.strictEqual(
+            isHasItem.includes("Remove"),
+            true,
+            "There is no remove button"
+        );
+      } finally {
+        setTimeout(async () => {
+          await driver.quit();
+        }, 5000);
+      }
+      })
   });
 }
 
