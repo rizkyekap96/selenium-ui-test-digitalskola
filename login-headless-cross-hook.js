02adc07be53774e6b72b58ead30768b73eb5320a
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-25 21:37:55 +0700

    fix: add click backpack in tc04 validate item on cart

--- login-headless-cross-hook.js
+++ login-headless-cross-hook.js
@@ -82,6 +82,7 @@ async function SauceDemo() {
           await driver.findElement(By.id("password")).sendKeys("secret_sauce");
           await driver.findElement(By.name("login-button")).click();
 
+          await driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
           await driver.findElement(By.id("shopping_cart_container")).click();
 
           let cartItems = await driver.findElements(By.css(".cart_item"));
