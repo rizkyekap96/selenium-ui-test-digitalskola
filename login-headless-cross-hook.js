fcadc4e70252ca7d3d79734ebd1503e6aeba0e02
Rizky Eka Pratama <rizkyekap96@gmail.com>
2025-02-25 22:01:47 +0700

    enhancement

--- login-headless-cross-hook.js
+++ login-headless-cross-hook.js
@@ -1,4 +1,4 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
+const { Builder, By} = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const firefox = require("selenium-webdriver/firefox");
 const edge = require("selenium-webdriver/edge");
@@ -11,15 +11,15 @@ async function SauceDemo() {
     const browsers = [
       {
         name: "chrome",
-        options: new chrome.Options().addArguments("--headless"),
+        options: new chrome.Options().addArguments("--headless")
       },
       {
         name: "firefox",
-        options: new firefox.Options().addArguments("--headless"),
+        options: new firefox.Options().addArguments("--headless")
       },
       {
         name: "MicrosoftEdge",
-        options: new edge.Options().addArguments("--headless"),
+        options: new edge.Options().addArguments("--headless")
       },
     ];
 
